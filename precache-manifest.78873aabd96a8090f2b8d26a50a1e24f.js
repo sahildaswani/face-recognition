b77b178cfb8ed1c74eb50bb98e25bb68554ae842
Sahil Daswani <sahildaswani@Sahils-MacBook-Pro-3.local>
2020-04-11 01:28:39 +0800

    Updates

---
+++ precache-manifest.78873aabd96a8090f2b8d26a50a1e24f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560b8f83ccd87cee05ab37bba861ffb4",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "46f3bde91b4c784983ab",
+    "url": "/face-recognition/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "7237e318fb1485c09d2d",
+    "url": "/face-recognition/static/css/main.9cfa1e61.chunk.css"
+  },
+  {
+    "revision": "46f3bde91b4c784983ab",
+    "url": "/face-recognition/static/js/2.def08313.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition/static/js/2.def08313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7237e318fb1485c09d2d",
+    "url": "/face-recognition/static/js/main.1e5a4154.chunk.js"
+  },
+  {
+    "revision": "20e380779a106829f3bf",
+    "url": "/face-recognition/static/js/runtime-main.1810cf19.js"
+  }
+]);
